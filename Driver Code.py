aa396f293e59a72693a8570709352d268f8f5a96
DJ <social.deej@gmail.com>
2021-04-07 23:51:30 +0530

    feat: Updates

--- Driver Code.py
+++ Driver Code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[44]:
+# In[1]:
 
 
 from selenium import webdriver
@@ -13,14 +13,14 @@ op.add_argument('headless')
 browser = webdriver.Chrome(executable_path='./chromedriver', options = op)
 
 
-# In[45]:
+# In[2]:
 
 
 initial_data = pd.read_excel('Stock-URL.xlsx')
 initial_data
 
 
-# In[46]:
+# In[3]:
 
 
 def rsi_value_of_given_stock ( stock_name, stock_url ):
@@ -30,24 +30,24 @@ def rsi_value_of_given_stock ( stock_name, stock_url ):
     monthly_rsi_url = stock_url + 'monthly'
     
     browser.get(daily_rsi_url)
-    daily_rsi_value = browser.find_element_by_xpath('//*[@id="techindd"]/div[3]/table/tbody[2]/tr[1]/td[2]/strong').text
+    daily_rsi_value = float( browser.find_element_by_xpath('//*[@id="techindd"]/div[3]/table/tbody[2]/tr[1]/td[2]/strong').text )
     
     browser.get(weekly_rsi_url)
-    weekly_rsi_value = browser.find_element_by_xpath('//*[@id="techind"]/div[3]/table/tbody[2]/tr[1]/td[2]/strong').text
+    weekly_rsi_value = float (browser.find_element_by_xpath('//*[@id="techind"]/div[3]/table/tbody[2]/tr[1]/td[2]/strong').text)
     
     browser.get(monthly_rsi_url)
-    monthly_rsi_value = browser.find_element_by_xpath('//*[@id="techind_m"]/div[3]/table/tbody[2]/tr[1]/td[2]/strong').text
+    monthly_rsi_value = float (browser.find_element_by_xpath('//*[@id="techind_m"]/div[3]/table/tbody[2]/tr[1]/td[2]/strong').text)
     
     # Date
     date_for_the_given_stock = browser.find_element_by_xpath('//*[@id="div_bse_livebox_wrap"]/div[1]/div[1]/div/div[1]/span').text
 
     # Closing
-    closing_value = browser.find_element_by_xpath('//*[@id="div_nse_livebox_wrap"]/div[1]/div[1]/div/div[2]/span[1]').text
+    closing_value = float (browser.find_element_by_xpath('//*[@id="div_nse_livebox_wrap"]/div[1]/div[1]/div/div[2]/span[1]').text)
     
     return [daily_rsi_value, weekly_rsi_value, monthly_rsi_value, date_for_the_given_stock, closing_value]
 
 
-# In[66]:
+# In[4]:
 
 
 # Adding Storing Index Values 
@@ -55,10 +55,11 @@ data_record = pd.read_excel('Record-Data.xlsx', header=0, index_col=0)
 data_record
 
 
-# In[67]:
+# In[5]:
 
 
 for index, row in initial_data.iterrows():
+    print (row["Name of Stock"])
     data_for_stock = rsi_value_of_given_stock (row["Name of Stock"], row["Url"])
     data_record.loc[row["Name of Stock"], "Daily as on {}".format(data_for_stock[3])] = data_for_stock[0]
     data_record.loc[row["Name of Stock"], "Weekly as on {}".format(data_for_stock[3])] = data_for_stock[1]
@@ -68,13 +69,13 @@ for index, row in initial_data.iterrows():
 data_record
 
 
-# In[69]:
+# In[6]:
 
 
 data_record.to_excel('Record-Data.xlsx')
 
 
-# In[39]:
+# In[7]:
 
 
 # # for index, row in initial_data.iterrows():
@@ -83,7 +84,7 @@ data_record.to_excel('Record-Data.xlsx')
 #     print (row["Name of Stock"], row["Url"])
 
 
-# In[71]:
+# In[8]:
 
 
 # Adding Storing Index Values 
@@ -91,11 +92,17 @@ data_record.to_excel('Record-Data.xlsx')
 # data_record
 
 
-# In[70]:
+# In[9]:
 
 
 # data_record.loc["SOmething", "Data as on asome data "] = 4
 # data_record
+
+
+# In[7]:
+
+
+# rsi_value_of_given_stock("Reliance", "https://www.moneycontrol.com/technical-analysis/relianceindustries/RI/")
 
 
 # In[ ]:
